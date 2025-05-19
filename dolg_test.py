e7c8beee1950d070b2dcf8caa7c83d5df19bd5d9
muravev47 <muravevm47@gmail.com>
2025-05-19 22:39:50 +0800

    Add tasks dolg_test.py

--- dolg_test.py
+++ dolg_test.py
@@ -3,3 +3,12 @@ from tkinter import ttk, messagebox
 import os
 
 TASK_FILE = "tasks.txt"
+
+def load_tasks():
+    tasks = []
+    if os.path.exists(TASK_FILE):
+        with open(TASK_FILE, "r") as f:
+            for line in f:
+                status, task = line.strip().split(";", 1)
+                tasks.append({"task": task, "status": status == "True"})
+    return tasks
